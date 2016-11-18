3036ec667d8e106e3db05166bf8f8f737279caa2
Sarah Vessels <cheshire137@gmail.com>
2016-11-18 14:21:05 -0600

    When clicking a reaction button, play sound

--- extension/content.js
+++ extension/content.js
@@ -1,5 +1,8 @@
 (function() {
   const src = chrome.extension.getURL('sounds/mario-coin.mp3')
-  const sound = new Howl({ src, volume: 0.5 })
-  sound.play()
+
+  $('body').on('click', '.js-reaction-option-item', function(event) {
+    const sound = new Howl({ src, volume: 0.5 })
+    sound.play()
+  })
 })()
